c8522abe1d886967c29ded0945498c92a899116a
Elias Volonakis <eliasvolonakis@Eliass-MacBook-Pro.local>
2025-09-04 14:22:38 -0400

    Updated questions table in with just embedding field

--- src/db/index.ts
+++ src/db/index.ts
@@ -80,8 +80,7 @@ const setupDatabase = async (): Promise<boolean> => {
                 "id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
                 "question" text NOT NULL,
                 "answer" text NOT NULL,
-                "question_embedding" vector(1536) NOT NULL,
-                "answer_embedding" vector(1536) NOT NULL,
+                "embedding" vector(1536) NOT NULL,
                 "created_at" timestamp DEFAULT now(),
                 "updated_at" timestamp DEFAULT now()
             )
